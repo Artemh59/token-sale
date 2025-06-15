f43b559a2edbcca5cb76e14707f442936edf1578
Artem <hartem472@gmail.com>
2025-06-15 08:01:14 +0200

    connect provider

---
+++ src/components/Providers/AppKitProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { createAppKit } from '@reown/appkit/react';
+import { EthersAdapter } from '@reown/appkit-adapter-ethers';
+import { mainnet } from '@reown/appkit/networks';
+
+const metadata = {
+  name: 'Best Token',
+  description: 'Buy the best token here',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/'],
+};
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+
+if (!projectId) {
+	throw new Error('ProjectId is not defiend!');
+}
+
+createAppKit({
+	adapters: [new EthersAdapter()],
+	metadata,
+	networks: [mainnet],
+	projectId,
+});
+export function AppKitProvider({ children }: { children: React.ReactNode }) {
+
+  return <>{children}</>;
+}
